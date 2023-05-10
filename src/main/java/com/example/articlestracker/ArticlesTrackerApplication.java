a06689186b5e620a53b8701a2dc13da9d66c52a1
Yuliia A <julialex38@gmail.com>
2023-05-10 21:41:51 +0300

    Initial commit

---
+++ src/main/java/com/example/articlestracker/ArticlesTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.articlestracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticlesTrackerApplication.class, args);
+	}
+
+}
